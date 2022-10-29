9c6dadcf45363f49a49276da4b87a39521ad9ea0
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-10-29 23:30:56 +0530

    file input logic

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let imgLoaded = false
+let imgLoaded = false;
+
+const fileInput = document.getElementById("file_selector");
+
+function selFile() {
+  fileInput.click();
+}
+
+function fileSelected() {
+  if (fileInput.files.length !== 0) {
+    const [file] = fileInput.files;
+    document.getElementById("prevImg").src = URL.createObjectURL(file);
+  }
+}
